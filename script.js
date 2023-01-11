2026b645ed3d2f2df6dba7f28c535cbbd6cc3c4c
Jackson Cummins <jscummins98@gmail.com>
2023-01-10 21:31:58 -0500

    add js for resetting form and remove button

--- script.js
+++ script.js
@@ -5,15 +5,12 @@ function Book (title, author, pages, read) {
   this.author = author
   this.pages = pages
   this.read = read
-  this.info = function () {
-    return title + ' by ' + author + ', ' + pages + ' pages, ' + read
-  }
 }
 
-const bookForm = document.querySelector('#book-form')
-let bookEntry
-
 // Function to create a new book from the form
+const bookForm = document.querySelector('#book-form')
+const checkbox = document.getElementById('checkbox')
+let bookEntry
 function addBookToLibrary (event) {
   event.preventDefault()
 
@@ -44,31 +41,32 @@ function displayBook (book) {
   bookCard.appendChild(pages)
 
   const readButton = document.createElement('button')
-  readButton.textContent += 'Incomplete'
   readButton.classList.add('read')
+  if (checkbox.checked) {
+    setComplete(readButton)
+  } else {
+    setIncomplete(readButton)
+  }
   readButton.addEventListener('click', () => {
     if (readButton.classList.contains('finished')) {
-      readButton.classList.remove('finished')
-      readButton.textContent = 'Incomplete'
+      setIncomplete(readButton)
     } else {
-      readButton.classList.add('finished')
-      readButton.textContent = 'Completed'
+      setComplete(readButton)
     }
   })
   bookCard.appendChild(readButton)
 
+  const removeButton = document.createElement('button')
+  removeButton.textContent += 'Remove'
+  removeButton.addEventListener('click', () => {
+    bookshelf.removeChild(bookCard)
+  })
+  bookCard.appendChild(removeButton)
+
   bookshelf.appendChild(bookCard)
 }
 
-
-
-
-
-
-
-
-
-// This code block displays the form
+// This code block displays, removes, and resets the form
 const addButton = document.querySelector('.new-button')
 const popUp = document.querySelector('.new-book-form')
 const cover = document.querySelector('.cover')
@@ -76,15 +74,37 @@ addButton.addEventListener('click', () => {
   cover.style.setProperty('display', 'block')
   popUp.style.setProperty('display', 'block')
 })
+
 const closeButton = document.querySelector('.close-button')
-closeButton.addEventListener('click', () => {
+closeButton.addEventListener('click', removeForm)
+
+function removeForm () {
   cover.style.setProperty('display', 'none')
   popUp.style.setProperty('display', 'none')
-})
+}
+
+function resetForm () {
+  bookForm.title.value = ''
+  bookForm.author.value = ''
+  bookForm.pages.value = ''
+  checkbox.checked = false
+}
 
 // This block gives the form submit button functionality
 const submitButton = document.querySelector('.submit-button')
 submitButton.addEventListener('click', addBookToLibrary)
+submitButton.addEventListener('click', removeForm)
+submitButton.addEventListener('click', resetForm)
+
+// These functions change the completed/incomplete buttons
+function setComplete (element) {
+  element.classList.add('finished')
+  element.textContent = 'Completed'
+}
+function setIncomplete (element) {
+  element.classList.remove('finished')
+  element.textContent = 'Incomplete'
+}
 
 // Adds default books to the bookshelf
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
